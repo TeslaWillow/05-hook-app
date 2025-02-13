88bbc004e777227a469bfe159ad367a9513114c6
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-12 21:46:09 -0600

    Custom Hook with form.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { SimpleWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SimpleForm />
+    <SimpleWithCustomHook />
   </StrictMode>,
 )
