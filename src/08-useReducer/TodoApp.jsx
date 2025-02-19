4ec9de79e7383b572b73c2c18eb35f935430e8c3
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-02-18 20:49:13 -0600

    show todos

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -23,14 +23,38 @@ export const TodoApp = () => {
 
     return (
         <>
-            <h1>Todo App</h1>
+            <h1>Todo App: 10, <small>Pendientes: 2</small></h1>
             <hr />
 
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group d-flex justify-content-between">
+                        {
+                            todos.map( (todo, i) => (
+                                <li key={ todo.id } className="list-group-item d-flex justify-content-between">
+                                    <p className="text-center">{ i + 1 }. { todo.description }</p>
+                                    <button className="btn btn-danger">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+
+                    <form action="">
+                        <input type="text" className="form-control" placeholder="¿Qué hay que hacer?" />
+                        <button type="submit" className="btn btn-outline-primary mt-1">
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            
         </>
     )
 }
