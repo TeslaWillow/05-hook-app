f1738336cf39c5a2921677e65342eeabf5134dc9
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-03-20 22:27:59 -0600

    Router tests

---
+++ test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,25 @@
+const { render, screen } = require("@testing-library/react");
+const { MainApp } = require("../../src/09-useContext/MainApp");
+const { MemoryRouter } = require("react-router");
+
+describe('Pruebas en <MainApp />', () => {
+
+    test('Debe de mostrar el HomePage', () => {
+        render(
+            <MemoryRouter>
+                <MainApp />
+            </MemoryRouter>
+        );
+        expect( screen.getByText('HomePage') ).toBeTruthy();
+    });
+
+    test('Debe de mostrar el LoginPage', () => {
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <MainApp />
+            </MemoryRouter>
+        );
+        expect( screen.getByText('LoginPage') ).toBeTruthy();
+    });
+
+});
